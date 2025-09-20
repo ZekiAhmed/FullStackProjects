cf0ccae5ce121cbbf53ca281e948e775407ad0db
zeki ahmed <changezworld3@gmail.com>
2025-09-21 00:38:03 +0300

    single page finished

--- pinterest-clone-MERN/client/src/components/comments/comments.jsx
+++ pinterest-clone-MERN/client/src/components/comments/comments.jsx
@@ -1,8 +1,61 @@
+import { useState } from 'react'
+import Image from '../image/image'
 import './comments.css'
+import EmojiPicker from 'emoji-picker-react'
 
 function Comments() {
+    const [open, setOpen] = useState(false)
     return (
-        <div className="comments">Comments</div>
+        <div className="comments">
+            <div className="commentList">
+                <span className='commentCount'>5 comments</span>
+                {/* COMMENT */}
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt="" />
+                    <div className="commentContent">
+                        <span className="commentUsername">Jhon Doe</span>
+                        <p className='commentText'>Lorem ipsum dolor sit amet consectetur.</p>
+                        <span className='commentTime'>1h</span>
+                    </div>
+                </div>
+                {/* COMMENT */}
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt="" />
+                    <div className="commentContent">
+                        <span className="commentUsername">Jhon Doe</span>
+                        <p className='commentText'>Lorem ipsum dolor sit amet consectetur.</p>
+                        <span className='commentTime'>1h</span>
+                    </div>
+                </div>
+                {/* COMMENT */}
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt="" />
+                    <div className="commentContent">
+                        <span className="commentUsername">Jhon Doe</span>
+                        <p className='commentText'>Lorem ipsum dolor sit amet consectetur.</p>
+                        <span className='commentTime'>1h</span>
+                    </div>
+                </div>
+                {/* COMMENT */}
+                <div className="comment">
+                    <Image path="/general/noAvatar.png" alt="" />
+                    <div className="commentContent">
+                        <span className="commentUsername">Jhon Doe</span>
+                        <p className='commentText'>Lorem ipsum dolor sit amet consectetur.</p>
+                        <span className='commentTime'>1h</span>
+                    </div>
+                </div>
+            </div>
+            <form className='commentForm'>
+                <input type="text" placeholder='Add a comment' />
+                <div className="emoji">
+                    <div onClick={()=>setOpen(prev=>!prev)}>🙂️</div>
+                    {open && <div className="emojiPicker">
+                        <EmojiPicker/>
+                    </div>}
+                </div>
+            </form>
+        </div>
     )
 }
 
--- pinterest-clone-MERN/client/src/components/postInteractions/postInteractions.jsx
+++ pinterest-clone-MERN/client/src/components/postInteractions/postInteractions.jsx
@@ -1,9 +1,14 @@
+import Image from '../image/image'
 import './postInteractions.css'
 
 function PostInteractions() {
     return (
         <div className="postInteractions">
-            PostInteractions
+            <div className="interactionIcons">
+                <Image path="/general/share.svg" alt="" />
+                <Image path="/general/more.svg" alt="" />
+            </div>
+            <button>Save</button>
         </div>
     )
 }
