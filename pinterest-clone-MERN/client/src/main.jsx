2b72be0436f686d57b3272890da17c654db05e29
zeki ahmed <changezworld3@gmail.com>
2025-11-13 18:04:09 +0300

    mainjsx update

--- pinterest-clone-MERN/client/src/main.jsx
+++ pinterest-clone-MERN/client/src/main.jsx
@@ -2,17 +2,19 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import MainLayout from './routes/layouts/mainLayout.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
 // import HomePage from './routes/homePage/homePage.jsx'
 // import CreatePage from './routes/createPage/createPage.jsx'
 // import PostPage from './routes/postPage/postPage.jsx'
 // import ProfilePage from './routes/profilePage/profilePage.jsx'
 // import SearchPage from './routes/searchPage/searchPage.jsx'
 // import AuthPage from './routes/authPage/authPage.jsx'
-import MainLayout from './routes/layouts/mainLayout.jsx'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 
 
 
