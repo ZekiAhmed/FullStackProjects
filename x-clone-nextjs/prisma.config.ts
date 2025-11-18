e83531c2b00c20e1961e73ffefae499cab081196
zeki ahmed <changezworld3@gmail.com>
2025-11-18 08:03:13 +0300

    prisma and seeding finished

--- x-clone-nextjs/prisma.config.ts
+++ x-clone-nextjs/prisma.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: `tsx prisma/seed.ts`,
   },
   engine: "classic",
   datasource: {
