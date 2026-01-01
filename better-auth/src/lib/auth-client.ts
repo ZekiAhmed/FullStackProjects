9e2b55795ba12be0b42fbab4ded1b0d14bae4706
Zeki Ahmed <changezworld3@gmail.com>
2026-01-01 23:31:32 +0300

    created session logic in navbar, dashboard,userdropdown

--- better-auth/src/lib/auth-client.ts
+++ better-auth/src/lib/auth-client.ts
@@ -1,7 +1,11 @@
 import { nextCookies } from 'better-auth/next-js'
 import { createAuthClient } from 'better-auth/react'
+import { inferAdditionalFields } from 'better-auth/client/plugins'
+import { auth } from './auth'
 
 // export const { signIn, signUp, signOut, useSession } = createAuthClient()
 export const authClient = createAuthClient({
-    plugins: [nextCookies()]
+    plugins: [
+        inferAdditionalFields<typeof auth>(),
+        nextCookies()]
 })
---
+++ better-auth/src/lib/get-session.ts
@@ -0,0 +1,13 @@
+import { headers } from "next/headers";
+import { cache } from "react";
+import { auth } from "./auth";
+
+export const getServerSession = cache(async () => {
+  console.log("getServerSession");
+  return await auth.api.getSession({ headers: await headers() });
+});
+
+// export async function getServerSession()  {
+//   console.log("getServerSession");
+//   return await auth.api.getSession({ headers: await headers() });
+// };
