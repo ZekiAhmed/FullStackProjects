cec6d97e543148c6d059ca9b7fb4768f64da667a
Zeki Ahmed <changezworld3@gmail.com>
2026-01-02 22:53:39 +0300

    resend is configured

--- better-auth/src/lib/auth.ts
+++ better-auth/src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '@/lib/prisma'
+import { sendEmail } from './email';
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -8,6 +9,17 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true,
+    },
+    emailVerification: {
+        sendOnSignUp: true,
+        autoSignInAfterVerification: true,
+        async sendVerificationEmail({ user, url }) {
+            await sendEmail({
+                to: user.email,
+                subject: "Verify your email",
+                text: `Click the link to verify your email: ${url}`,
+            });
+        },
     },
     user: {
         additionalFields: {
